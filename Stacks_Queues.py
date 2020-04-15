94867bba81a23a44ec6f7354bdb7ff659d8e1983
Khalil Kum <kum.vanjunior@gmail.com>
2020-04-14 22:16:10 -0400

    Update Stacks_Queues.py

--- Stacks_Queues.py
+++ Stacks_Queues.py
@@ -21,3 +21,43 @@ def isBalanced(s):
         return "NO"
     else:
         return "YES"
+
+## Implement a stack object ##
+
+class Stack(object):
+    def __init__(self):
+        self.stack = []        
+    
+    def peek(self):
+        if len(self.stack) < 1:
+            return []
+        tail = len(self.stack)-1
+        return self.stack[tail]        
+        
+    def pop(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack.pop()        
+        
+    def put(self, value):
+        self.stack.append(value)
+        
+        
+## Implement a queue object with a stack methods ##
+
+class Queue(object):
+    def __init__(self):
+        self.stack = []        
+    
+    def peek(self):
+        if len(self.stack) < 1:
+            return []
+        return self.stack[0]        
+        
+    def pop(self):
+        if len(self.stack) < 1:
+            return None
+        return self.stack.pop(0)        
+        
+    def put(self, value):
+        self.stack.append(value)
