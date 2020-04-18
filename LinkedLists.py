e7db2dd560aa8343e5f64229d1ce56eb66848f09
Khalil Kum <kum.vanjunior@gmail.com>
2020-04-17 23:08:45 -0400

    Update

--- LinkedLists.py
+++ LinkedLists.py
@@ -16,3 +16,40 @@ def insertNodeAtPosition(head, data, position):
     currentNode.next = newNode                 
 
     return head
+
+
+## Insert a new node into a sorted doublylinked list such that the list is still sorted afterwards ##
+# For your reference:
+#
+# DoublyLinkedListNode:
+#     int data
+#     DoublyLinkedListNode next
+#     DoublyLinkedListNode prev
+
+def sortedInsert(head, data):
+    newNode = DoublyLinkedListNode(data)
+    currentNode = head
+
+    if currentNode.data > newNode.data:
+        newNode.next = currentNode
+        currentNode.prev = newNode
+        head = newNode
+        return head
+
+    
+    while True:
+        if currentNode.next is None:
+            currentNode.next = newNode
+            newNode.prev = currentNode
+            break
+        elif currentNode.next.data >= newNode.data:
+            newNode.next = currentNode.next
+            currentNode.next = newNode
+            currentNode.next.prev = newNode
+            newNode.prev = currentNode
+            break
+        currentNode = currentNode.next
+
+    return head
+
+
