14b494f6e0a5440bae59508ff15cec6685779d9f
Khalil Kum <kum.vanjunior@gmail.com>
2020-04-20 00:00:04 -0400

    intersection

--- LinkedLists.py
+++ LinkedLists.py
@@ -112,3 +112,52 @@ def findLoop(head):
         fastRunner = fastRunner.next
 
     return fastRunner
+
+## Find the Intersection between two linked lists ##
+# Lists are gauranteed to have an intersection
+def findMergeNode(head1, head2):
+    node1 = head1
+    node2 = head2
+    count1 = count2 = 0
+
+    while node1:
+        count1 += 1
+        node1 = node1.next
+    
+    while node2:
+        count2 += 1
+        node2 = node2.next
+    
+    node1 = head1
+    node2 = head2
+    
+    if count1 > count2:
+        d = count1 - count2
+        i =0
+        while i < d:
+            node1 = node1.next
+            i += 1
+        while node1 and node2:
+            if node1 == node2:
+                return node1.data
+            node1 = node1.next
+            node2 = node2.next
+    elif count2 > count1:
+        d = count2 - count1
+        i =0
+        while i < d:
+            node2 = node2.next
+            i += 1
+        while node1 and node2:
+            if node1 == node2:
+                return node1.data
+            node1 = node1.next
+            node2 = node2.next
+    else:
+        while node1 and node2:
+            if node1 == node2:
+                return node1.data
+            node1 = node1.next
+            node2 = node2.next
+        
+    return None
